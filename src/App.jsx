df5859c89cb21e30d4bf18185355dd9839b61e7f
Colon0911 <calfaromedina@gmail.com>
2022-06-10 16:48:05 -0600

    version1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AuthLayouts from './layouts/AuthLayouts';
-import Login from './pages/Login';
-import ForgetPassword from './pages/ForgetPassword';
-import ResetPassword from './pages/ResetPassword';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayouts from "./layouts/AuthLayouts";
+import Login2 from "./pages/login2";
+import ForgetPassword from "./pages/ForgetPassword";
+import ResetPassword from "./pages/ResetPassword";
 
 //Primer Route= Area Publica
 function App() {
@@ -10,13 +10,13 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AuthLayouts />}>
-          <Route index element={<Login />}/>
-          <Route path="forget-password" element={<ForgetPassword />}/>
-          <Route path="reset-password/:token" element={<ResetPassword />}/>
+          <Route index element={<Login2 />} />
+          <Route path="forget-password" element={<ForgetPassword />} />
+          <Route path="reset-password/:token" element={<ResetPassword />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/login2.jsx
+++ src/pages/login2.jsx
@@ -1,38 +1,116 @@
-import React, { useState } from 'react'
-import { Formik } from 'formik';
-import { BasicForm } from '../../components';
-import "../build/css/app.css"
+import { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import "../build/css/app.css";
+import Alerta from "./Alerta";
 
+const login2 = () => {
+  const inicioSeccionShema = Yup.object().shape({
+    email: Yup.string()
+      .email("Email no valido")
+      .required("El email es obligatorio"),
+    password: Yup.string().required("Es requerida la contraseña"),
+  });
 
-const validation = values => {
-    let errors = {};
-   
-    if (!values.email) {
-      errors.name = 'Name is required!';
-    } else if (values.name.length <= 1) {  
-      errors.name = 'Name has to be 1 character at less!';
-    }
-    
-    return errors;
-   }
-   
-   const Basic = () => (
-    <Formik
-      initialValues={{
-        email: '',
-        password: '',
-      }}
-      onSubmit={(values, actions) => {
-        setTimeout(() => {
-          console.log(values)
-          console.log(actions)
-          actions.setSubmitting(false);
-        }, 2000);
-      }}
-      validate={validation}
-    >
-      {props => <BasicForm {...props} />}
-    </Formik>
-   );
-   
-   export default Basic;
+  const [validValues, setvalidValues] = useState(false);
+
+  const handleSubmit = async (values) => {
+    console.log(values);
+    values.preventDefault();
+
+    // {
+    //   values && values ? (
+    //     <Alerta type="success"> inicio de seccion exitoso </Alerta>
+    //   ) : null;
+    // }
+
+    // try {
+    //     const url = 'http://localhost:4000/clientes'
+    //     const response = await fetch(url, {
+    //         method: 'POST',
+    //         body: JSON.stringify(values),
+    //         headers: {
+    //             "Content-Type": 'application/json'
+    //         }
+    //     });
+    //     const resultado = await response.json();
+    //     console.log(resultado);
+    // } catch (error) {
+    //     console.log(error);
+    // }
+  };
+
+  return (
+    <>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={(values) => {
+          handleSubmit(values);
+        }}
+        validationSchema={inicioSeccionShema}
+      >
+        {({ errors, touched }) => {
+          return (
+            <div className="senara-dashboard">
+              <form className="senara-content-sm-login" onSubmit={handleSubmit}>
+                <div className="senara-logo">
+                  <div className="senara-img-logo" alt="" />
+                </div>
+
+                <p className="senara-tagline">Iniciar Sesion</p>
+                <p className="senara-description-page">
+                  Ingrese sus credenciales
+                </p>
+
+                <Field
+                  id="email"
+                  type="email"
+                  className="loating-input:focus~.bar"
+                  placeholder="Usuario o Correo"
+                  name="email"
+                />
+                {errors.email && touched.email ? (
+                  <Alerta type="error"> {errors.email} </Alerta>
+                ) : null}
+
+                <Field
+                  id="password"
+                  type="password"
+                  className="loating-input:focus~.bar"
+                  placeholder="Contraseña
+"
+                  name="password"
+                />
+                {errors.password && touched.password ? (
+                  <Alerta type="error"> {errors.password} </Alerta>
+                ) : null}
+
+                <button className="senara-btn-primary" type="submit">
+                  Iniciar Secciòn
+                </button>
+
+                <div className="senara-actions">
+                  <a className="a" href="">
+                    Crear cuenta
+                  </a>
+                  <a className="a" href="">
+                    ¿Olvidò la contraseña?
+                  </a>
+                </div>
+              </form>
+
+              <footer className="senara-footer-decoration">
+                <div className="decoration-logo"></div>
+              </footer>
+            </div>
+          );
+        }}
+      </Formik>
+    </>
+  );
+};
+
+export default login2;
